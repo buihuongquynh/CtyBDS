a3e9e7527a5d225d56ba09c28f0473810794d832
bui huong quynh <buigiangquynh@gmail.com>
2022-03-19 16:36:26 +0700

    up

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       Địa chỉ  :  74 Thôi Hữu, phường Ngọc Trạo, thành phố Thanh Hóa, Việt Nam &ensp; | &ensp; Điện thoại : 039 615 8898 (0372559079) &ensp; | &ensp;   Email : Phothietke@gmail.com <br/>
       </p>
       <a target="_blank" href="https://www.facebook.com/Ph%E1%BB%91-Thi%E1%BA%BFt-K%E1%BA%BF-101002412514369/?ref=pages_you_manage">Facebook</a><br/>
-      <a target="_blank">Intergram</a>
+      <a target="_blank" href="https://www.instagram.com/phothietke/">Intergram</a>
     </div>
   );
 };
