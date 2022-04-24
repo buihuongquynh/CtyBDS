3b00e417772315839c41ae796266dcfa9c8705aa
buihuongquynh <buigiangquynh@gmail.com>
2022-04-24 09:51:48 +0700

    fixFooter

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "be620072b8e30a3b"; }
+/******/ 		__webpack_require__.h = function() { return "251acd7eea68d872"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
