04a2cc80b893f66a91f698872dbdb06206466d62
zHieuNguyenz <95176969+zHieuNguyenz@users.noreply.github.com>
2022-05-20 08:09:40 +0700

    Update index.js

--- assets/img/projectImg/Show room dược phẩm/Show room dược phẩm/index.js
+++ assets/img/projectImg/Show room dược phẩm/Show room dược phẩm/index.js
@@ -1,7 +1,8 @@
-export const medicine1 = require('./0.jpg');
-export const medicine2 = require('./1.jpg');
-export const medicine3 = require('./2.jpg');
-export const medicine4 = require('./3.jpg');
-export const medicine5 = require('./4.jpg');
+
+export const medicine1 = require('./1.jpg');
+export const medicine2 = require('./2.jpg');
+export const medicine3 = require('./3.jpg');
+export const medicine4 = require('./4.jpg');
+export const medicine5 = require('./5.jpg');
 export const medicine6 = require('./6.jpg');
 export const medicine7 = require('./7.jpg');
