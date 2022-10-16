41fdb21688772942cf881ba66cede048039bd4ee
buihuongquynh <buigiangquynh@gmail.com>
2022-10-16 11:50:54 +0700

    fix

--- assets/img/projectImg/cafeNhahang/Cfnga5/index.js
+++ assets/img/projectImg/cafeNhahang/Cfnga5/index.js
@@ -1,7 +1,7 @@
-export const AnCoffe1 = require('./1.jpg');
-export const AnCoffe2 = require('./2.jpg');
-export const AnCoffe3 = require('./3.jpg');
-export const AnCoffe4 = require('./4.jpg');
-export const AnCoffe5 = require('./5.jpg');
-export const AnCoffe6 = require('./6.jpg');
+export const Cfnga51 = require('./1.jpg');
+export const Cfnga52 = require('./2.jpg');
+export const Cfnga53 = require('./3.jpg');
+export const Cfnga54 = require('./4.jpg');
+export const Cfnga55 = require('./5.jpg');
+export const Cfnga56 = require('./6.jpg');
 
